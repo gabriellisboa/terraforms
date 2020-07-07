fe8aec727b66d0c2caf105c7d4d3fd4f89cf9a14
Gabiroto <gabriel.sl@outlook.com>
2020-07-06 21:07:51 -0300

    Fixing ecs cluster state

--- providers/aws/services/ecs/cluster/main.tf
+++ providers/aws/services/ecs/cluster/main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     region  = "us-east-1"
     bucket  = "terraforms-gabiroto"
-    key     = "tf-states/asg-app.tfstate"
+    key     = "tf-states/ecs-service.tfstate"
     encrypt = true
   }
 }
